01f0c7a69be14b7f0f1918c3ed432b4ec39e87e1
Lucifer Lu <yuntao.lu@gmail.com>
2012-12-09 20:07:38 +0800

    remove console.log

--- contentscript.js
+++ contentscript.js
@@ -2,7 +2,6 @@ $("img").each(function(index){
     var thiz = $(this);
     var img = new Image();
     img.onload = function() {
-        console.log(this.width + 'x' + this.height);
         if(this.width >= 1000 || this.height >= 1000 ){
             $(thiz).attr('Xw', this.width).width(this.width/2);
             chrome.extension.sendRequest({}, function(response) {});
