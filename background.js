3a13bb4348da427b4055436f3f7a0922fb75e203
Lucifer Lu <yuntao.lu@gmail.com>
2012-12-09 21:55:25 +0800

    reduce event generated

--- background.js
+++ background.js
@@ -1,11 +1,6 @@
-var iconDisplyed = false;
-
 function onRequest(request, sender, sendResponse) {
-    if(!iconDisplyed){
-        iconDisplyed = true;
-        chrome.pageAction.show(sender.tab.id);
-        //sendResponse({});
-    }
+    chrome.pageAction.show(sender.tab.id);
+    //sendResponse({});
 }
 
 chrome.extension.onRequest.addListener(onRequest);
--- contentscript.js
+++ contentscript.js
@@ -1,10 +1,15 @@
+var iconDisplayed = false;
+
 $("img").each(function(index){
     var thiz = $(this);
     var img = new Image();
     img.onload = function() {
         if(this.width >= 1000 || this.height >= 1000 ){
             $(thiz).attr('Xw', this.width).width(this.width/2);
-            chrome.extension.sendRequest({}, function(response) {});
+            if(!iconDisplayed){
+                chrome.extension.sendRequest({}, function(response) {});
+                iconDisplayed = true;
+            }
         }
     };
     img.src = $(this).attr('src');
