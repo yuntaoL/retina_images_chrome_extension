4b479259a888807e56d3acc77e8d732c882efb30
Lucifer Lu <yuntao.lu@gmail.com>
2012-12-09 23:29:26 +0800

    * set height value specifically * add title attribute when change image scale

--- contentscript.js
+++ contentscript.js
@@ -5,7 +5,11 @@ $("img").each(function(index){
     var img = new Image();
     img.onload = function() {
         if(this.width >= 1000 || this.height >= 1000 ){
-            $(thiz).attr('Xw', this.width).width(this.width/2);
+            halfWidth = this.width/2;
+            halfHeight = this.height/2;
+            $(thiz).attr('Xw', this.width).width(halfWidth);
+            $(thiz).attr('Xh', this.height).height(halfHeight);
+            $(thiz).attr('title', this.width + "x" + this.height + " => " + halfWidth + "x" + halfHeight);
             if(!iconDisplayed){
                 chrome.extension.sendRequest({}, function(response) {});
                 iconDisplayed = true;
